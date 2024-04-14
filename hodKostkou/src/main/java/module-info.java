984b38e426c1ffd8ee588328a345a88227e7fe1b
Daniel Bušek <139177894+danielbusek@users.noreply.github.com>
2024-04-14 18:46:43 +0200

    Delete hodKostkou directory

--- hodKostkou/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.hodkostkou {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.hodkostkou to javafx.fxml;
-    exports com.example.hodkostkou;
-}
